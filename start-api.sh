c13c9845a19625d1993445830eb8f69225a49a73
gisman <gisman@gmail.com>
2024-12-26 20:46:45 +0900

    fix: start-api.sh에서 Flask 대신 Uvicorn 사용으로 변경

--- start-api.sh
+++ start-api.sh
@@ -9,8 +9,9 @@ cd ~/projects/geocoder-kr
 source venv/bin/activate
 echo 'Starting...'
 
-export FLASK_ENV=production
-echo | nohup python api_fast.py 4002 >> log/geocode-api.log &
+# export FLASK_ENV=production
+# echo | nohup python api_fast.py 4002 >> log/geocode-api.log &
+echo | nohup uvicorn --host=0.0.0.0 --port=4002 api_fast:app >> log/geocode-api.log &
 
 sleep 1
 echo 'Started'
